34d22ade3f05c2910386f383ad86c6ef218aa910
abhinava31 <abhinava@hornet.technology>
2024-09-20 16:52:32 +0530

    check access on localnetwork

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,7 @@ func main() {
 	store := idis.NewInMemoryRepository()
 
 	// Create a new TCP server
-	srv := server.NewServer(":1234", store)
+	srv := server.NewServer("0.0.0.0:1234", store) //you can change the address and port here to your desired address and port
 
 	// Run the server
 	if err := srv.Run(); err != nil {
